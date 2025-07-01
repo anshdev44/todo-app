d4854828478944de0e1e9d8da60013c6b33914f7
anshdev44 <anshahuja770@gmail.com>
2025-07-01 22:28:14 +0530

    added some logic to the code

--- src/app.jsx
+++ src/app.jsx
@@ -3,30 +3,18 @@ import './app.css'
 
 export function App() {
   const [input, setinput] = useState("")
+  const [check, setCheck] = useState(false);
+
+function completed() {
+  setCheck(!check);
+}
 
   function add() {
     if (input.trim() === "") {
       alert("add something");
       return;
     } else {
-      document.querySelector('.todos').insertAdjacentHTML('beforeend', `
-  <div class="todo1">
-    <div class="todo1_desc">
-      <p>${input}</p>
-    </div>
-    <div class="delete_todo">
-      <svg width="32" height="32" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        <rect x="5" y="6" width="14" height="14" fill="#FF3B3B" rx="1" ry="1" />
-        <rect x="7" y="8" width="2" height="10" fill="black" />
-        <rect x="11" y="8" width="2" height="10" fill="black" />
-        <rect x="15" y="8" width="2" height="10" fill="black" />
-        <rect x="9" y="2" width="6" height="2" fill="#FF3B3B" />
-        <rect x="8" y="4" width="8" height="2" fill="#FF3B3B" />
-      </svg>
-    </div>
-  </div>
-`);
-
+      document.querySelector('.todos').insertAdjacentHTML('beforeend', ``);
       setinput("");
     }
   }
@@ -70,9 +58,38 @@ export function App() {
           </div>
         </div>
 
-        
-        <div className="todos">
-         {/* ✅ Add the todos container */}
+
+        <div className="todos" style={{ display: check ? "none" : "flex" }}>
+          <div class="todo1">
+            <div class="todo1_desc">
+              <p>hi</p>
+            </div>
+            <div className="contains">
+            <div className="completed_task">
+              <svg width="24" height="24" onClick={completed} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <rect x="2" y="13" width="2" height="2" fill="white" />
+                <rect x="4" y="15" width="2" height="2" fill="white" />
+                <rect x="6" y="17" width="2" height="2" fill="white" />
+                <rect x="8" y="15" width="2" height="2" fill="white" />
+                <rect x="10" y="13" width="2" height="2" fill="white" />
+                <rect x="12" y="11" width="2" height="2" fill="white" />
+                <rect x="14" y="9" width="2" height="2" fill="white" />
+                <rect x="16" y="7" width="2" height="2" fill="white" />
+              </svg>
+
+            </div>
+            <div class="delete_todo">
+              <svg width="32" height="32" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <rect x="5" y="6" width="14" height="14" fill="#FF3B3B" rx="1" ry="1" />
+                <rect x="7" y="8" width="2" height="10" fill="black" />
+                <rect x="11" y="8" width="2" height="10" fill="black" />
+                <rect x="15" y="8" width="2" height="10" fill="black" />
+                <rect x="9" y="2" width="6" height="2" fill="#FF3B3B" />
+                <rect x="8" y="4" width="8" height="2" fill="#FF3B3B" />
+              </svg>
+            </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
